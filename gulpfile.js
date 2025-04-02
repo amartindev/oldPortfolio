061958c0f47d5739f9d5253bc9ca912a8c4ce881
Antonio Martin <37764339+amartindev@users.noreply.github.com>
2025-04-01 20:31:43 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require("gulp-sass")(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require("gulp-plumber");
 
+
 function css(done) {
     src('sass/style.scss')
         .pipe(plumber())
@@ -13,16 +14,22 @@ function css(done) {
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(dest('css'));
+        .pipe(dest('dist/css'));
     done();
 }
+
+function copyAssets(done) {
+    src('assets/**/*')
+        .pipe(dest('dist/assets'));
+    done();
+}
+
 
 function dev(done) {
     watch('sass/**/*.scss', css);
     done();
 }
 
-
-exports.build = series(css);
+exports.build = series(css, copyAssets);
 exports.css = css;
 exports.dev = parallel(dev);
